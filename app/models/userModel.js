20fa587288cd3e1b57912c9605adc13d4c7974d4
insovaideveloper <insovaidev@gmail.com>
2023-11-21 09:51:29 +0700

    'updated'

--- app/models/userModel.js
+++ app/models/userModel.js
@@ -93,7 +93,7 @@ module.exports = {
         // Where condition
         if(filters){
             if(filters.sid) q.where('s.sid', '>', filters.sid)    
-            if(filters.not_port && filters.not_port == 1) q.whereRaw('port IS NULL')
+            if(filters.not_port && filters.not_port == 1) q.where('port', NULL)
         }
 
         // Return 
